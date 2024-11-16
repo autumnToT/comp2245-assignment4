17990a40c40071388b7d5388ba4996a7557677d4
Autumn Charles <autumn.m.s.c@gmail.com>
2024-11-16 08:42:26 -0500

    Added the alert for the list of super heroes from the php file

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //app.js
+
+document.getElementById('searchButton').addEventListener('click', function () {
+  fetch('superheroes.php')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.text();
+    })
+    .then(html => {
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = html;
+
+      const items = tempDiv.querySelectorAll('li');
+      const superheroList = Array.from(items).map(item => item.textContent).join('\n');
+
+      alert('Superheroes:\n' + superheroList);
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+});
+
